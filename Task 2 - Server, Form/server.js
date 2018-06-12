e0d17498cc61ccdd623a45c90c42270b883cafb8
Natalia Podolyaka <natalia.podolyaka@ukr.net>
2018-06-12 21:54:06 +0300

    Add task 2 - implementation of server

---
+++ Task 2 - Server, Form/server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(requestHandler);
+
+function requestHandler (request, response) {
+    const { headers, method, url } = request;
+    switch (method) {
+        case 'GET':
+            let stream = fs.createReadStream ('./index.html');
+            stream.pipe(response);
+            break;
+        case 'POST':
+            let body = [];
+            request.on('data', (chunk) => {
+                body.push(chunk);
+                body = body.toString();
+            });
+            request.on('end', () => {
+                body = JSON.stringify(body);
+                fs.writeFile("./data.txt", body, 'utf8', function (err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    response.write("The file was saved!");
+                    response.end();
+                });
+            });
+            break;
+        default:
+            response.statusCode = 404;
+            response.end();
+            break;
+    }
+}
+
+server.listen(8080);
